73237c0a4bd71e34e5fd5e1055c6aa596aa521e0
Jon Pacheco <jon.pacheco2@gmail.com>
2020-11-25 13:05:17 +0000

    feat: use monospace font + adjust max chars

--- typetest-iced/src/theme.rs
+++ typetest-iced/src/theme.rs
@@ -1,6 +1,11 @@
-use iced::{container, Color};
+use iced::{Color, Font, container};
 
 pub mod default_light;
+
+const NOTO_SANS_MONO: Font = Font::External {
+    name: "Noto Sans Mono",
+    bytes: include_bytes!("../fonts/NotoSansMono/NotoSansMono-Regular.ttf")
+};
 
 #[derive(Copy, Clone, Debug)]
 pub enum Theme {
@@ -14,6 +19,10 @@ pub struct TextPalette {
 }
 
 impl Theme {
+    pub const fn monospace() -> Font {
+        NOTO_SANS_MONO
+    }
+
     pub const fn text_palette(&self) -> &TextPalette {
         match *self {
             Theme::DefaultLight => &default_light::TEXT_PALETTE,
--- typetest-iced/src/widgets/typing_test.rs
+++ typetest-iced/src/widgets/typing_test.rs
@@ -18,7 +18,7 @@ use crate::{
     AppMessage,
 };
 
-const MAX_CHARS: usize = 60;
+const MAX_CHARS: usize = 55;
 
 /// Represents the possible messages that could be sent during a typing test.
 #[derive(Clone, Debug)]
@@ -238,7 +238,7 @@ impl TypingTestState {
                 WordStatus::Incorrect => text_palette.incorrect,
             };
 
-            Text::new(&word.word).color(color)
+            Text::new(&word.word).color(color).font(Theme::monospace())
         }
 
         fn words_to_displayed_row<'a>(
