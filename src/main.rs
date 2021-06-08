6cce5bffbd2b5d270553ec5b184538848749c83a
Jon Pacheco <jon.pacheco@tutanota.com>
2021-06-08 20:57:48 +0100

    fix: hide console window in release mode
    missed this since I was developing on linux.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use iced::{Align, Application, Column, Container, Length, Text};
 use typetest_themes::{ApplicationTheme, Theme};
 use views::{
