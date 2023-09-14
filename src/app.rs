8564ea4df74dc327b0bdc07cf4acf3b56c67551f
Jon Pacheco <jon.pacheco@tutanota.com>
2023-09-14 16:11:39 +0100

    chore: setup skeleton project with iced 0.10

---
+++ src/app.rs
@@ -0,0 +1,37 @@
+use iced::{widget, Application, Command, Element, Subscription};
+
+pub struct App {}
+
+#[derive(Debug, Clone)]
+pub enum Message {}
+
+impl Application for App {
+    type Executor = iced::executor::Default;
+    type Message = Message;
+    type Theme = iced::Theme;
+    type Flags = ();
+
+    fn new(_flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
+        (App {}, Command::none())
+    }
+
+    fn title(&self) -> String {
+        "TypeTest".to_string()
+    }
+
+    fn theme(&self) -> Self::Theme {
+        iced::Theme::Dark
+    }
+
+    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn subscription(&self) -> Subscription<Self::Message> {
+        Subscription::none()
+    }
+
+    fn view(&self) -> Element<'_, Self::Message, iced::Renderer<Self::Theme>> {
+        widget::text("Hello world!").into()
+    }
+}
